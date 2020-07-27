b6c1567a2878db648a7eca9bec430f1693fde2f7
Vinicius Botto <vinibotto@gmail.com>
2020-07-27 19:44:10 +0100

    added migration

--- webapitutorial/Startup.cs
+++ webapitutorial/Startup.cs
@@ -45,8 +45,9 @@ namespace webapitutorial
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommanderContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
